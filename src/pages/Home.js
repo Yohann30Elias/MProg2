31a387c0d59e06700ad2ecb0c774f0778b6a9fdb
Yohann30Elias <yriond30@gmail.com>
2024-11-08 14:30:14 +0100

    csschanges

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,45 @@ import "../style/Home.css";
 function Home() {
   return (
     <div className="Homestyle">
-      <h1>Homepage</h1>
-      <p>
-        das ist die Homepage
-      </p>
+
+      <div className="parallax"></div>
+
+      <div className="geschichte">
+        <h1>Geschichte</h1>
+        <p>
+          Pfungen, erstmals 993 n. Chr. urkundlich erwähnt, blickt auf eine lange Geschichte zurück. 
+          Der Name stammt vom altdeutschen „Funginga“, was „bei den Leuten des Fungo“ bedeutet. 
+          Ursprünglich ein Bauerndorf, prägten Landwirtschaft und die Nähe zur Töss die Entwicklung Pfungens. 
+          Im Mittelalter gehörte es teils zum Kloster Einsiedeln und teils zu den Grafen von Kyburg, 
+          bis es schließlich unter Zürcher Herrschaft kam.
+
+          Mit der Industrialisierung im 19. Jahrhundert wandelte sich Pfungen: Textil- und Maschinenindustrie 
+          prägten die Region, und die Bevölkerung wuchs. Der Anschluss an das Bahnnetz 1855 verstärkte diesen Wandel 
+          und machte das Dorf zu einem beliebten Wohnort für Pendler. Heute verbindet Pfungen modernes Leben mit Tradition; 
+          das historische Dorfzentrum und alte Gebäude wie die reformierte Kirche erinnern an seine Vergangenheit. 
+          In den letzten Jahrzehnten hat sich das Dorf kontinuierlich entwickelt, bleibt aber stark mit seiner Geschichte und Natur verbunden.
+        </p>
+      </div>
+
+      <div className="heute">
+        <h1>Heute</h1>
+        <p>
+          balabablalabl
+        </p>
+      </div>
+
+      <div className="parallax"></div>
+
+      <div className="aktuell">
+        <h1>Aktuelles</h1>
+        <p>
+          balabablalabl
+        </p>
+      </div>
+
+      <div className="parallax"></div>
+
+
     </div>
   );
 }
