cefe298d376fae5ad06864f3e8bf969d7e1ab9c6
Yohann30Elias <yriond30@gmail.com>
2024-11-08 14:52:21 +0100

    parafax

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,21 +7,23 @@ function Home() {
 
       <div className="parallax"></div>
 
-      <div className="geschichte">
-        <h1>Geschichte</h1>
-        <p>
-          Pfungen, erstmals 993 n. Chr. urkundlich erwähnt, blickt auf eine lange Geschichte zurück. 
-          Der Name stammt vom altdeutschen „Funginga“, was „bei den Leuten des Fungo“ bedeutet. 
-          Ursprünglich ein Bauerndorf, prägten Landwirtschaft und die Nähe zur Töss die Entwicklung Pfungens. 
-          Im Mittelalter gehörte es teils zum Kloster Einsiedeln und teils zu den Grafen von Kyburg, 
-          bis es schließlich unter Zürcher Herrschaft kam.
+      <div className="geschichte-container">
+        <div className="geschichte">
+          <h1>Geschichte</h1>
+          <p>
+            Pfungen, erstmals 993 n. Chr. urkundlich erwähnt, blickt auf eine lange Geschichte zurück. 
+            Der Name stammt vom altdeutschen „Funginga“, was „bei den Leuten des Fungo“ bedeutet. 
+            Ursprünglich ein Bauerndorf, prägten Landwirtschaft und die Nähe zur Töss die Entwicklung Pfungens. 
+            Im Mittelalter gehörte es teils zum Kloster Einsiedeln und teils zu den Grafen von Kyburg, 
+            bis es schließlich unter Zürcher Herrschaft kam.
 
-          Mit der Industrialisierung im 19. Jahrhundert wandelte sich Pfungen: Textil- und Maschinenindustrie 
-          prägten die Region, und die Bevölkerung wuchs. Der Anschluss an das Bahnnetz 1855 verstärkte diesen Wandel 
-          und machte das Dorf zu einem beliebten Wohnort für Pendler. Heute verbindet Pfungen modernes Leben mit Tradition; 
-          das historische Dorfzentrum und alte Gebäude wie die reformierte Kirche erinnern an seine Vergangenheit. 
-          In den letzten Jahrzehnten hat sich das Dorf kontinuierlich entwickelt, bleibt aber stark mit seiner Geschichte und Natur verbunden.
-        </p>
+            Mit der Industrialisierung im 19. Jahrhundert wandelte sich Pfungen: Textil- und Maschinenindustrie 
+            prägten die Region, und die Bevölkerung wuchs. Der Anschluss an das Bahnnetz 1855 verstärkte diesen Wandel 
+            und machte das Dorf zu einem beliebten Wohnort für Pendler. Heute verbindet Pfungen modernes Leben mit Tradition; 
+            das historische Dorfzentrum und alte Gebäude wie die reformierte Kirche erinnern an seine Vergangenheit. 
+            In den letzten Jahrzehnten hat sich das Dorf kontinuierlich entwickelt, bleibt aber stark mit seiner Geschichte und Natur verbunden.
+          </p>
+        </div>
       </div>
 
       <div className="heute">
